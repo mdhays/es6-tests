fa91fa94f4c96552fc318a42f2417ee626d120b7
Matt Hays <mdhays1995@gmail.com>
2016-01-25 15:28:28 -0600

    added a new test

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -6,4 +6,27 @@ describe('template strings', function() {
   var string ='abc';
      expect(string).to.be.a('string');
   });
+
+  it('can evaluate variables', function() {
+    var bar = `baz`;
+    var string = `foo${bar}`;
+
+    expect(string).to.equal('foobaz');
+  });
+
+  it('can execute functions', function() {
+    var string = `the date is: ${new Date()}`;
+    var date = new Date();
+
+    expect(string).to.equal(`the date is: ` + date);
+  })
+
+  it('can evaluate an expression', function() {
+    var expression = `1 + 2 = ${1 + 2}`;
+
+    expect(expression).to.equal(`1 + 2 = 3`);
+  });
 });
+
+
+
